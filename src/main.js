ac898e881fadf387fe57bf1b58023c1a51023db5
Ciao Chung <ciao0958@gmail.com>
2018-11-17 16:01:38 +0800

    init base app, add axios & momentjs

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
 import chalk from 'chalk'
-console.log(chalk.cyan('App start...'))
-export default 'foo'
+import { CronJob } from 'cron'
+class App {
+  constructor() {
+    global.chalk = chalk
+    global.log = this.log
+    console.log(chalk.cyan('App start...'))
+    this.Job = new CronJob('* * * * * *', this.start)
+    this.Job.start()
+  }
+
+  /**
+   * log style(white, red, green, yellow, cyan, magenta)
+   */
+  log(content, style = 'cyan') {
+    console.log(chalk[`${style}Bright`](content)+chalk.whiteBright(`\t at ${now()}`))
+  }
+
+  start() {
+    console.log(chalk.cyan(new Date()))
+  }
+
+
+}
+
+export default new App()
